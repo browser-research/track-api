0220477e2305dbee31cb2696e153b2fa25e377eb
Konstantin.Lebejko <konstantin.lebejko@gmail.com>
2020-09-28 23:38:15 +0300

    fix: updated mimtype for /scripts/collection to avoid firefox alerts

--- routes/scripts.py
+++ routes/scripts.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
 
 scripts_blueprint = Blueprint("scripts", __name__,)
 
 
 @scripts_blueprint.route("/scripts/collection")
 def collection():
-    return current_app.collection_content
+    response = make_response(current_app.collection_content)
+    response.mimetype = "application/javascript"
+    return response
